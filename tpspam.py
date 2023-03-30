48f0c171546d2bebd69d1449ae5f513a0842f10d
zinedine <zine>
2023-03-30 18:40:50 +0200

    prediction and test

--- tpspam.py
+++ tpspam.py
@@ -24,8 +24,8 @@ def charge_dico(fichier):
 	mots = f.read().split("\n")
 	print("Chargé " + str(len(mots)) + " mots dans le dictionnaire")
 	f.close()
-	
-	return mots[:-1]
+	motsPlusDe3lettre = [mot for mot in mots[:-1] if len(mot) >= 3]
+	return motsPlusDe3lettre
 
 def apprendBinomial(dossier, fichiers, dictionnaire):
 	"""
@@ -37,12 +37,12 @@ def apprendBinomial(dossier, fichiers, dictionnaire):
 	
 	occurencesMots = np.zeros(len(dictionnaire))
 	for fichier in fichiers:
-		mail = lireMail(fichier,dictionnaire)
+		mail = lireMail(dossier+"/"+fichier,dictionnaire)
 		occurencesMots = occurencesMots + mail
 
 	#Lissage des paramètres 
 	b = ((occurencesMots + epsilon)/ (nb+2*epsilon ))
-	return b   # vecteur de paramètres binomiaux
+	return b  # vecteur de paramètres binomiaux
 
 
 def prediction(x, Pspam, Pham, bspam, bham):
@@ -50,12 +50,20 @@ def prediction(x, Pspam, Pham, bspam, bham):
 		Prédit si un mail représenté par un vecteur booléen x est un spam
 		à partir du modèle de paramètres Pspam, Pham, bspam, bham.
 		Retourne True ou False.
-		
 	"""
+
+	#calcul de P(Y=Spam | X=x) 
+	#Pspam_x= math.log(Pspam) + np.sum(np.log((bspam ** x) * ((1-bspam) ** (1-np.array(x)))))
+	Px_spam = np.prod((bspam ** x) * ((1-bspam) ** (1-np.array(x))))
+	Pspam_x=  Pspam * Px_spam
 	
+	#calcul de P(Y=Ham | X=x)
+	#Pham_x = math.log(Pham) + np.sum(np.log((bham ** x) * ((1-bham) ** (1-np.array(x)))))
+	Px_ham = np.prod( (bham ** x) * ((1-bham) ** (1-np.array(x))))
+	Pham_x=  Pham * Px_ham
 
-
-
+	if (Pspam_x >= Pham_x):
+		return True
 	return False  # à modifier...
 	
 def test(dossier, isSpam, Pspam, Pham, bspam, bham):
@@ -67,13 +75,20 @@ def test(dossier, isSpam, Pspam, Pham, bspam, bham):
 		Retourne le taux d'erreur 
 	"""
 	fichiers = os.listdir(dossier)
+	nbErreur = 0 
+	dictionnaire=  charge_dico("dictionnaire1000en.txt")
 	for fichier in fichiers:
-		print("Mail " + dossier+"/"+fichier)		
+		res = prediction(lireMail(dossier+"/"+fichier,dictionnaire),Pspam, Pham, bspam, bham)
+		if (res+isSpam)%2 == 1:
+			print(("SPAM" if isSpam else "HAM") +" " + dossier+"/"+fichier + " identifié comme un "+("SPAM" if (not isSpam) else "HAM")+" *** erreur ***")		
+		else:
+			print(("SPAM" if isSpam else "HAM") +" "+dossier+"/"+fichier + " identifié comme un "+("SPAM" if (isSpam) else "HAM"))		
 
-		
-		# à compléter...
+		if (res+isSpam)%2 == 0:
+			nbErreur = nbErreur +1  
 
-	return 0  # à modifier...
+	tauxErreur = nbErreur/len(fichiers)
+	return 1-tauxErreur # à modifier...
 
 
 ############ programme principal ############
@@ -81,8 +96,8 @@ def test(dossier, isSpam, Pspam, Pham, bspam, bham):
 dossier_spams = "spam/baseapp/spam"	# à vérifier
 dossier_hams = "spam/baseapp/ham"
 
-fichiersspams = list(map(lambda e : dossier_spams+"/"+ e ,os.listdir(dossier_spams)))  
-fichiershams = list(map(lambda e : dossier_hams+"/"+ e ,os.listdir(dossier_hams)))  
+fichiersspams = os.listdir(dossier_spams)
+fichiershams =  os.listdir(dossier_hams) 
 
 mSpam = len(fichiersspams)
 mHam = len(fichiershams)
@@ -98,14 +113,16 @@ bspam = apprendBinomial(dossier_spams, fichiersspams, dictionnaire)
 print("apprentissage de bham...")
 bham = apprendBinomial(dossier_hams, fichiershams, dictionnaire)
 
-print(bspam)
-#print(bham)
-
 
 # Calcul des probabilités a priori Pspam et Pham:
 Pspam = mSpam/(mSpam+mHam) 
 Pham =  mHam/(mSpam+mHam)
 
 # Calcul des erreurs avec la fonction test():
+erreurSpam =test("spam/baseTest/spam",True,Pspam,Pham,bspam,bham)
+erreurHam = test("spam/baseTest/ham",False,Pspam,Pham,bspam,bham)
 
+print("Erreur de test sur 500 SPAM      : " +str(round(erreurSpam, 4)*100) +"%")
+print("Erreur de test sur 500 HAM       : " +str(round(erreurHam, 4)*100) +"%")
+print("Erreur de test globale sur 600 mails   : " +str((round(erreurSpam, 4)*100+round(erreurHam, 4)*100)/2) +"%")
 
